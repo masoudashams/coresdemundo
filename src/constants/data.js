4b9d338581748c3d24c9aa533c2ae1ea7be5333e
SebghatYusuf <sebghatyusuf@gmail.com>
2022-08-28 22:32:59 +0430

    add cards

---
+++ src/constants/data.js
@@ -0,0 +1,55 @@
+const images = [];
+const carditems = [
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car2.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Word',
+    image: "/images/car3.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'New organism',
+    image: "/images/car2.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+  {
+    title: 'Hello',
+    image: "/images/car1.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non officia eos! Quia magnam aperiam, quaerat corporis esse aut molestiae accusamus qui inventore repudiandae dolorum sunt, ullam nihil. Inventore, deserunt!"
+  },
+];
+
+
+
+export {
+  images,
+  carditems,
+};
